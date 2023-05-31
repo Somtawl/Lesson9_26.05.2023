f17ed678c7236e44e352f02e97ba076ca8d8c1d0
Somtawl <somtawl@yandex.ru>
2023-05-31 21:05:16 +0300

    Сделал ДЗ

---
+++ task40&42/main.py
@@ -0,0 +1,8 @@
+#Задача 40: Работать с файлом california_housing_train.csv, который находится в папке
+#sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
+
+df[df['population']<=500]['median_house_value'].mean()
+
+#Задача 42: Узнать какая максимальная households в зоне минимального значения population
+
+df[df['population']==df['population'].min()]['households'].max()
